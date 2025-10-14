b55a7f1ecda60cb62139e02910d18d0cd2d345fa
Md Sarwar Jahan Moon <93179023+sarwarjr70@users.noreply.github.com>
2025-10-14 18:27:39 +0600

    factorial_using_loop_01.py
    update_factorial

--- factorial_using_loop.py
+++ factorial_using_loop.py
@@ -1,12 +1,21 @@
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-factorial = 1
+# factorial = 1
 
-if num < 0:
-   print("factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+# if num < 0:
+#    print("factorial does not exist for negative numbers")
+# elif num == 0:
+#    print("The factorial of 0 is 1")
+# else:
+#    for i in range(1,num + 1):
+#        factorial = factorial*i
+#    print("The factorial of",num,"is",factorial)
+
+
+n = int(input("Enter a number: "))
+
+product = 1
+
+for i in range(1, n + 1):
+      product *= i
+print(f"The factorial of {n} is: {product}")
